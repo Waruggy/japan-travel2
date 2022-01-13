d1961285bd3e5f69fba0cc753936c955fb39cf67
Waruggy <cwang8413@yahoo.com>
2022-01-13 05:34:38 -0800

    Put some pages

---
+++ pages/Blog.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+     <div class="row">
+          <!-- Image with text -->
+          <div class="col">
+            <ImageTemplate :source="'mtfuji.jpg'" :message="'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'" :title="'Mt. Fuji'" :desc="'Image of Mt. Fuji'"/>
+            <ImageTemplate :source="'tokyo.jpg'" :message="'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'" :title="'Tokyo'" :desc="'Image of Tokyo'"/>
+          </div>
+      </div>
+      <!-- Footer -->
+    <div class="row bg-light text-middle">
+      &copy;2021 Japan Travel, LLC | Seattle, WA
+    </div>
+  </div>
+</template>
+
+<script>
+import ImageTemplate from '../components/Image.vue'
+export default {
+  name: 'BlogPage',
+  components: {ImageTemplate},
+  paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import "../assets/scss/_base.normalize.scss";
+@import "../assets/scss/_components.content.scss";
+@import "../assets/scss/_settings.responsive.scss";
+@import "../assets/scss/_settings.variables.scss";
+@import "../assets/scss/style.scss";
+</style>
---
+++ pages/Prefectures.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <div class="m-3">
+    <!-- Create two accordions each with a prefecture -->
+    <div class="accordion" id="prefectures">
+        <div class="accordion-item"  v-for="content in prefecture" v-bind:key="content">
+          <h2 class="accordion-header" id="headingOne" >
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" data-toggle="tooltip" data-placement="top" title="Click to view">
+              {{content.name}}
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" :aria-labelledby="content.heading" data-bs-parent="#prefectures">
+            <div class="accordion-body">
+              <strong>{{content.text}}}</strong>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item" v-for="content in prefectureTwo" v-bind:key="content">
+          <h2 class="accordion-header" id="headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              {{content.name}}
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#prefectures">
+            <div class="accordion-body">
+                <strong>{{content.text}}</strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Button that when pressed displays an input text for -->
+    <div class="m-3" v-if="show">
+    <button type="button" name="button" v-on:click="show = !show"> Add Your Comment</button>
+    </div>
+    <div class="accordion-item" v-else>
+      <button type="button" name="button" v-on:click="show = !show"> Submit </button>
+      <input type="text" id="pname" name="pname"><br><br>
+      </div>
+      <!-- Footer -->
+      <div class="row bg-light text-center">
+       &copy;2021 Japan Travel, LLC | Seattle, WA
+    </div>
+  </div>
+</template>
+
+<script>
+// Store page content
+export default {
+    name: 'pageData',
+    data () {
+        return {
+            prefecture:[
+              { name: 'Tokyo' , text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+              ],
+              prefectureTwo: [
+              { name: 'Osaka' , text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+              ],
+              show: true,
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+// Formatting for page content
+button {
+  text-align: center;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: red;
+}
+</style>
